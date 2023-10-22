a53369d473f68f649f3bf600558fa22203e286f5
Muhammed Basheer <basheerkomassery@gmail.com>
2023-10-23 03:31:38 +0530

    Created stats endpoint to get statistical data related to jobs.

--- app/job/urls.py
+++ app/job/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     ListCreateJobView,
-    ManageJobView
+    ManageJobView,
+    getTopicStats
 )
 
 urlpatterns = [
     path('jobs/', ListCreateJobView.as_view(), name='jobs'),
-    path('jobs/<int:id>', ManageJobView.as_view(), name='manage_job')
+    path('jobs/<int:id>', ManageJobView.as_view(), name='manage_job'),
+    path('stats/<str:topic>/', getTopicStats, name='get_topic_stats'),
 ]
