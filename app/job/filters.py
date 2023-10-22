dded51ac3d79d49310446897c3f1588e82c714af
Muhammed Basheer <basheerkomassery@gmail.com>
2023-10-23 03:58:33 +0530

    added additional fields to enable search facility.

--- app/job/filters.py
+++ app/job/filters.py
@@ -11,10 +11,22 @@ class JobsFilter(filters.FilterSet):
         This class helps to filter jobs based
         on bellow defined criteria.
     """
+
+    keyword = filters.CharFilter(
+        field_name='title',
+        lookup_expr='icontains'
+    )
+
+    location = filters.CharFilter(
+        field_name='address',
+        lookup_expr='icontains'
+    )
+
     min_salary = filters.NumberFilter(
         field_name='salary' or 0,
         lookup_expr='gte'
     )
+
     max_salary = filters.NumberFilter(
         field_name='salary' or 10000000,
         lookup_expr='lte'
