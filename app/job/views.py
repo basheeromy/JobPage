22a8acc53d173809c5ed38c9a7ceb6528d3699b2
Muhammed Basheer <basheerkomassery@gmail.com>
2023-10-23 03:40:59 +0530

    Edited ListCreateJobView to include filters and added getTopicStats function to handle stats endpoint which generates statistics.

--- app/job/views.py
+++ app/job/views.py
@@ -1,16 +1,19 @@
 """
     Views to handle jobs
 """
-
 from django.shortcuts import get_object_or_404
-
+from django.db.models import Min, Max, Avg, Count
+from django_filters import rest_framework as filters
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView
 )
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .serializers import JobSerializer
 from .models import Job
+from .filters import JobsFilter
 
 
 class ListCreateJobView(ListCreateAPIView):
@@ -19,6 +22,7 @@ class ListCreateJobView(ListCreateAPIView):
     """
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+    filterset_class = JobsFilter
 
 
 class ManageJobView(RetrieveUpdateDestroyAPIView):
@@ -37,3 +41,27 @@ class ManageJobView(RetrieveUpdateDestroyAPIView):
             pk=self.kwargs["id"]
         )
         return obj
+
+@api_view(['GET'])
+def getTopicStats(request, topic):
+    """
+    get statics for search topics from database.
+    """
+
+    args = {'title__icontains': topic}
+    jobs = Job.objects.filter(**args)
+    print(jobs)
+    if len(jobs) == 0:
+        return Response({
+            'message': f'No data found for {topic}'
+        })
+
+    stats = jobs.aggregate(
+        total_jobs = Count('title'),
+        avg_positions = Avg('positions'),
+        avg_salary = Avg('salary'),
+        min_salary = Min('salary'),
+        max_salary = Max('salary')
+    )
+
+    return Response(stats)
