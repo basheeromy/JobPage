715ec150fb406b044b253efea2f587e2bb19f5f2
Muhammed Basheer <basheerkomassery@gmail.com>
2023-10-24 02:08:24 +0530

    Edited files to restric access to perform different operations.

--- app/job/views.py
+++ app/job/views.py
@@ -10,8 +10,10 @@ from rest_framework.generics import (
 )
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import permissions
 
-
+from permissions import custom_permissions
 from .serializers import JobSerializer
 from .models import Job
 from .filters import JobsFilter
@@ -22,10 +24,20 @@ class ListCreateJobView(ListCreateAPIView):
     """
     View to create and list all jobs.
     """
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Job.objects.all()
     serializer_class = JobSerializer
     filterset_class = JobsFilter
     pagination_class = CustomPagination
+
+    def perform_create(self, serializer):
+        """
+        perform_create method will help us to
+        assign request.user to user field of job model
+        before create method is called.
+        """
+        serializer.save(user=self.request.user)
 
 
 class ManageJobView(RetrieveUpdateDestroyAPIView):
@@ -34,6 +46,11 @@ class ManageJobView(RetrieveUpdateDestroyAPIView):
     get, put, patch, delete methods
     """
 
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [
+        permissions.IsAuthenticated,
+        custom_permissions.IsOwnerOrReadOnly
+    ]
     serializer_class = JobSerializer
     queryset = Job.objects.all()
     lookup_field = 'id'
@@ -43,7 +60,12 @@ class ManageJobView(RetrieveUpdateDestroyAPIView):
             self.get_queryset(),
             pk=self.kwargs["id"]
         )
+        self.check_object_permissions(
+            self.request,
+            obj
+        )
         return obj
+    
 
 @api_view(['GET'])
 def getTopicStats(request, topic):
