d2a2a0ed87a68060070e15279d17d0e45218bf86
Muhammed Basheer <basheerkomassery@gmail.com>
2023-10-30 22:24:51 +0530

    Changes made to render list of jobs fetched from backend api.

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -3,7 +3,9 @@ import Link from 'next/link';
 
 import JobItem from './job/JobItem';
 
-const Home = () => {
+const Home = ({ data }) => {
+  const {results, count, resPerPage } = data
+  const jobs = data.results
   return (
     <div className="container container-fluid">
         <div className="row">
@@ -23,8 +25,7 @@ const Home = () => {
                 <Link href="/search">Go to Search</Link>
               </div>
             </div>
-            <JobItem />
-            <JobItem />
+            {jobs && jobs.map((job) => <JobItem key={job.id} job={job} />) }
           </div>
         </div>
       </div>
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    API_URL: 'http://localhost:8001'
+  },
+};
 
 module.exports = nextConfig
