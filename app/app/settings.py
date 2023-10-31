263756bf53a8193580b4584f9cce1c391ce97643
Muhammed Basheer <basheerkomassery@gmail.com>
2023-10-31 17:38:12 +0530

    Edited settings of jwt token life for eas of development.

--- app/app/settings.py
+++ app/app/settings.py
@@ -168,8 +168,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=35),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
