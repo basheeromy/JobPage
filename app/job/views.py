a76589783508ea41fd26274cf0de374e784b794e
Muhammed Basheer <basheerkomassery@gmail.com>
2023-10-31 17:40:00 +0530

    Edited ManageJobView to add custom authentication with perform_authentication function.

--- app/job/views.py
+++ app/job/views.py
@@ -4,18 +4,21 @@
 from django.shortcuts import get_object_or_404
 from django.db.models import Min, Max, Avg, Count
 from django.utils import timezone
+from django.contrib.auth.models import AnonymousUser
 
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
     ListAPIView,
 )
+from rest_framework.request import Request
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import permissions
+from rest_framework.exceptions import AuthenticationFailed
 
 from permissions import custom_permissions
 from .serializers import (
@@ -34,8 +37,8 @@ class ListCreateJobView(ListCreateAPIView):
     """
     View to create and list all jobs.
     """
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [permissions.IsAuthenticated]
     queryset = Job.objects.all()
     serializer_class = JobSerializer
     filterset_class = JobsFilter
@@ -55,12 +58,7 @@ class ManageJobView(RetrieveUpdateDestroyAPIView):
     Manage jobs.
     get, put, patch, delete methods
     """
-
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [
-        permissions.IsAuthenticated,
-        custom_permissions.IsOwnerOrReadOnly
-    ]
+    permission_classes = [custom_permissions.IsOwnerOrReadOnly]
     serializer_class = JobSerializer
     queryset = Job.objects.all()
     lookup_field = 'id'
@@ -75,6 +73,24 @@ class ManageJobView(RetrieveUpdateDestroyAPIView):
             obj
         )
         return obj
+
+    def perform_authentication(self, request):
+        """
+        Customized authentication system.
+        """
+        # allow anonymous users to access data with safe methods
+        if request.method in permissions.SAFE_METHODS:
+            return
+
+        # check authentication for other methods.
+        if type(request.user) != AnonymousUser:
+            jw_at = JWTAuthentication()
+            auth_user, _  = jw_at.authenticate(request)
+            if auth_user:
+                return
+
+        raise AuthenticationFailed('Please provide valid authentication credentials.')
+
 
 
 @api_view(['GET'])
