d93cbe95d7ce4995d7b0d796697b73c1ec536fe7
Muhammed Basheer <basheerkomassery@gmail.com>
2023-11-01 23:06:23 +0530

    Created custom 404 not found page.

--- frontend/pages/job/[id].js
+++ frontend/pages/job/[id].js
@@ -1,29 +1,43 @@
-import Layout from "@/components/layout/Layout"
-import JobDetails from "@/components/job/JobDetails"
-
+import Layout from "@/components/layout/Layout";
+import JobDetails from "@/components/job/JobDetails";
 
 import axios from 'axios'
+import NotFound from "@/components/layout/NotFound";
 
-export default function JobDetailsPage({ job, candidates }) {
+export default function JobDetailsPage({ job, candidates, error }) {
+
+    if(error?.includes('Not found')) return <NotFound/>
+
     return (
-        <Layout>
+        <Layout title={job.title}>
             <JobDetails job={job} candidates={candidates} />
         </Layout>
     )
 }
 
 export async function getServerSideProps({ params }) {
-    const id = parseInt(params.id)
-    const res = await axios.get(`http://app:8001/api/jobs/${id}`)
-    const job = res.data.job
-    const candidates = res.data.candidates
+
+    try {
+        const id = parseInt(params.id)
+        const res = await axios.get(`http://app:8001/api/jobs/${id}`)
+        const job = res.data.job
+        const candidates = res.data.candidates
 
 
-    return {
-        props: {
-            job,
-            candidates
+        return {
+            props: {
+                job,
+                candidates
+            }
+        }
+
+    } catch (error) {
+        return {
+            props: {
+                error: error.response.data.detail,
+            }
         }
     }
+
 
 }
