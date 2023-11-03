af2cc97ad73a2111652832fad786d3971b020d01
Muhammed Basheer <basheerkomassery@gmail.com>
2023-11-03 18:40:00 +0530

    Edited pagination class to customise and provide required data.

--- app/job/paginations.py
+++ app/job/paginations.py
@@ -3,7 +3,17 @@
 """
 
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
-    page_size = 100
+    page_size = 2
     page_query_param = 'pa'
+
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'resPerPage': self.page.paginator.per_page,
+            'count': self.page.paginator.count,
+            'jobs': data
+        })
