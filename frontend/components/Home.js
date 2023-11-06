ba25bc7f7ffc229222ce7ed8f48da12a6b26f5cd
Muhammed Basheer <basheerkomassery@gmail.com>
2023-11-06 14:20:38 +0530

    Added filter feature.

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Pagination from 'react-js-pagination';
 
 import JobItem from './job/JobItem';
+import Filters from './layout/Filters';
 import { useRouter } from 'next/router';
 
 const Home = ({ data }) => {
@@ -32,7 +33,7 @@ const Home = ({ data }) => {
     <div className="container container-fluid">
         <div className="row">
           <div className="col-xl-3 col-lg-4">
-            {/* <Filters />{" "} */}
+            <Filters />
           </div>
 
           <div className="col-xl-9 col-lg-8 content-left-offset">
@@ -73,7 +74,7 @@ const Home = ({ data }) => {
           </div>
         </div>
       </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -13,11 +13,23 @@ export default function Index({data}) {
 
 export async function getServerSideProps({query}) {
 
+  const jobType = query.jobType || "";
+  const education = query.education || "";
+  const experience = query.experience || "";
   const keyword = query.keyword || ''
   const location = query.location || ''
   const page = query.page || 1;
 
-  const queryStr = `keyword=${keyword}&location=${location}&pa=${page}`
+  let min_salary = "";
+  let max_salary = "";
+
+  if (query.salary) {
+    const [min, max] = query.salary.split("-");
+    min_salary = min;
+    max_salary = max;
+  }
+  
+  const queryStr = `keyword=${keyword}&location=${location}&page=${page}&jobType=${jobType}&education=${education}&experience=${experience}&min_salary=${min_salary}&max_salary=${max_salary}`
 
   const res = await axios.get(`http://app:8001/api/jobs?${queryStr}`)
   const data = res.data
